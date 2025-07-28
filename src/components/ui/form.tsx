eb4979a18923dcb51117b1fb264a811bef51cde2
FO Kanu <Francisokore15@gmail.com>
2025-07-28 14:17:24 +0200

    fix: Update UI components to use separated variants and hooks, resolving Fast Refresh warnings

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -13,6 +13,7 @@ import {
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
 
+
 const Form = FormProvider
 
 type FormFieldContextValue<
@@ -39,29 +40,6 @@ const FormField = <
   )
 }
 
-const useFormField = () => {
-  const fieldContext = React.useContext(FormFieldContext)
-  const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
-
-  if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>")
-  }
-
-  const { id } = itemContext
-
-  return {
-    id,
-    name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
-    ...fieldState,
-  }
-}
-
 type FormItemContextValue = {
   id: string
 }
@@ -83,6 +61,8 @@ const FormItem = React.forwardRef<
   )
 })
 FormItem.displayName = "FormItem"
+
+import { useFormField } from "./form-hooks"
 
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
@@ -165,7 +145,6 @@ const FormMessage = React.forwardRef<
 FormMessage.displayName = "FormMessage"
 
 export {
-  useFormField,
   Form,
   FormItem,
   FormLabel,
